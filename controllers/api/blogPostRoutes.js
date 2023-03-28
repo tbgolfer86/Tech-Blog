396f6d068d463c7523165af195ba8ac7caf28223
tbgolfer86 <tbj86@outlook.com>
2023-03-28 13:13:26 -0400

    added PUT route to update blog post

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -15,6 +15,26 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const BlogPostData = await BlogPost.update(
+      {
+        title: req.body.title,
+        contents: req.body.content
+      },
+      {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    res.status(200).json(BlogPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const BlogPostData = await BlogPost.destroy({
--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,29 +1,30 @@
-// const editPostHandler = async (event) => {
-//     event.preventDefault();
+const editPostHandler = async (event) => {
+    event.preventDefault();
   
-//     const title = document.querySelector('#edit-post-title').value.trim();
-//     const content = document.querySelector('#edit-post-content').value.trim();
+    const title = document.querySelector('#edit-post-title').value.trim();
+    const content = document.querySelector('#edit-post-content').value.trim();
   
-//     if (title && content) {
-//       const response = await fetch(`/api/blogposts/:id`, {
-//         method: 'PUT',
-//         body: JSON.stringify({ title, content }),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
+    if (title && content) {
+      const id = event.target.getAttribute('data-id');
+      const response = await fetch(`/api/blogposts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
   
-//       if (response.ok) {
-//         document.location.replace('/dashboard');
-//       } else {
-//         alert('Failed to edit post');
-//       }
-//     }
-//   };
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to edit post');
+      }
+    }
+  };
   
-//   document
-//     .querySelector('.edit-post')
-//     .addEventListener('submit', editPostHandler);
+  document
+    .querySelector('.edit-post')
+    .addEventListener('submit', editPostHandler);
 
 
 
