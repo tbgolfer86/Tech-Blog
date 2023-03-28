e1995402cecf781cab45346956609283a0feb76f
tbgolfer86 <tbj86@outlook.com>
2023-03-28 12:37:26 -0400

    added delete blogpost functionality

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -24,11 +24,6 @@ router.delete('/:id', withAuth, async (req, res) => {
       },
     });
 
-    if (!BlogPostData) {
-      res.status(404).json({ message: 'No BlogPost found with this id!' });
-      return;
-    }
-
     res.status(200).json(BlogPostData);
   } catch (err) {
     res.status(500).json(err);
--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,36 +1,35 @@
-const editPostHandler = async (event) => {
-    event.preventDefault();
+// const editPostHandler = async (event) => {
+//     event.preventDefault();
   
-    const title = document.querySelector('#edit-post-title').value.trim();
-    const content = document.querySelector('#edit-post-content').value.trim();
+//     const title = document.querySelector('#edit-post-title').value.trim();
+//     const content = document.querySelector('#edit-post-content').value.trim();
   
-    if (title && content) {
-      const response = await fetch(`/api/blogposts/:id`, {
-        method: 'PUT',
-        body: JSON.stringify({ title, content }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+//     if (title && content) {
+//       const response = await fetch(`/api/blogposts/:id`, {
+//         method: 'PUT',
+//         body: JSON.stringify({ title, content }),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to edit post');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/dashboard');
+//       } else {
+//         alert('Failed to edit post');
+//       }
+//     }
+//   };
   
-  document
-    .querySelector('.edit-post')
-    .addEventListener('submit', editPostHandler);
+//   document
+//     .querySelector('.edit-post')
+//     .addEventListener('submit', editPostHandler);
 
 
 
 const delPostHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
-      
+      console.log(id)
       const response = await fetch(`/api/blogposts/${id}`, {
         method: 'DELETE',
       });
@@ -41,7 +40,6 @@ const delPostHandler = async (event) => {
         alert('Failed to delete post');
       }
     }
-  };
 
   document
     .querySelector('#delete')
